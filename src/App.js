6f8064abb5e11594a03cff71c4f486dbba385380
mesayaaksr <mesaya.ksr@gmail.com>
2024-03-12 19:26:52 +0700

    update Route path /home

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
 import UILogin from './Page/UILogin';
+import UIHome from "./Page/UIHome";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path='/' element={<UILogin/>} />
+        <Route path="/home" element={<UIHome/>}/>
       </Routes>
     </Router>
   );
